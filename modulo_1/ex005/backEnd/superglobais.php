2b598d19d0216817a14fb45b62e357f6cca6ada8
EduardoMoreiraDeSouza <63083065+EduardoMoreiraDeSouza@users.noreply.github.com>
2023-06-17 19:13:23 -0300

    Prática das aulas

---
+++ modulo_1/ex005/backEnd/superglobais.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Superglobais do PHP</title>
+
+    <link rel="stylesheet" href="../estilos/style.css">
+
+</head>
+
+<body>
+
+    <!-- Superglobais
+
+        $_GET[''] Através da Query Strig
+        $_POST['']
+        $_REQUEST['']
+        $_COOKIE['']
+        $_FILES['']
+        $_SESSION['']
+        $_ENV['']
+        $_SERVER['']
+        $GLOBALS['']
+
+    -->
+
+    <main>
+        <pre>
+            <?php
+
+                setcookie('dia-da-semana', 'segunda', time() + 3600);
+                session_start();
+
+                $_SESSION['teste'] = "Funcionou";
+
+                echo "<h1>Superglobal GET</h1>";
+                var_dump($_GET);
+
+                echo "<h1>Superglobal POST</h1>";
+                var_dump($_POST);
+
+                echo "<h1>Superglobal REQUEST</h1>";
+                var_dump($_REQUEST);
+
+                echo "<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+
+                /* foreach (getenv() as $c => $v) {
+                    echo " <br/> $c - $v";
+                } */
+
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo "<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
+
+            ?>
+        </pre>
+    </main>
+
+</body>
+
+</html>
